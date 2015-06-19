3aeb4adb181a3bcdb6903184dfff3574c5860078
sunquan8094 <sunquan8094@gmail.com>
2015-06-19 11:39:56 -1000

    Added support for regex OR

--- fbmsgscript.sh
+++ fbmsgscript.sh
@@ -2,18 +2,22 @@
 # fbmsgscript.sh
 
 filename="messages.htm"
+filefound=$(ls $1 | grep $filename | wc -l)
 
-if [ -n $1 -a -n $2 ]; then
+if [[ -n $1 ]] && [[ -n $2 ]] && [[ $filefound -eq 1 ]]; then
   sum=0
   for i in ${*:2:(($#-1))}; do 
+    actualregex=$(echo $i | sed -e 's/\|/\\\|/g')
     cnt=$(cat $1$filename | sed 's/<p>/\
 /g' | sed 's/<\/p>/\
 /g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed 's/ /\
-/g' | grep -e $i | wc -l)
+/g' | grep -e "$actualregex" | wc -l)
       ((sum+=cnt))
-    echo "There are ${cnt} instances of the regex $i in your Facebook exchanges."
+    echo "There are ${cnt} instances of the regex $actualregex in your Facebook exchanges."
   done
-  echo "I found ${sum} instances total" 
-else
+  echo "I found ${sum} instances total." 
+elif [[ -z $2 ]] || [[ -z $1 ]]; then
   echo "You're missing parameters, yo."
+elif [[ $filefound -le 0 ]]; then
+  echo "I could not find the file in the specified directory." 
 fi
