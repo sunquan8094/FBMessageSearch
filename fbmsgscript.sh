f198a110a3edac1e0abe4977643480bd72b6cd9f
sunquan8094 <sunquan8094@gmail.com>
2015-06-19 20:52:22 -1000

    Added a debug mode and modified primary command

--- fbmsgscript.sh
+++ fbmsgscript.sh
@@ -2,23 +2,33 @@
 # fbmsgscript.sh
 
 filename="messages.htm"
+debug=0
+if [[ ${!#} == "debug" ]]; then
+  debug=1
+fi
 
 if [[ $# -ge 2 ]]; then
   filefound=$(ls "$1" | grep "$filename" -c)
   if [[ filedfound > 0 ]]; then
     sum=0
-    for i in ${*:2:(($#-1))}; do
+    k=$(($#-1))
+    if [[ $debug == 1 ]]; then
+      k=$(($#-2))
+    fi
+    for i in ${*:2:$k}; do
       actualregex=$(echo $i | sed -e 's/\|/\\\|/g' | sed -e 's/\?/\\\?/g' | sed -e 's/</\\\</g' | sed -e 's/>/\\\>/g')
 
-  # Code below is used for debugging.
-  #   cat $1$filename | sed 's/<p>/\
-#/g' | sed 's/<\/p>/\
-#/g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed "s/&#039;/\'/g" | sed 's/&quot;/\"/g'| grep "$actualregex" | grep -v '^$'
+#  Code below is used for debugging.
+    if [[ $debug == 1 ]]; then
+     cat $1$filename | sed 's/<p>/\
+/g' | sed 's/<\/p>/\
+/g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed "s/&#039;/\'/g" | sed 's/&quot;/\"/g'| grep -o "$actualregex"
+    fi
 
       cnt=$(cat $1$filename | sed 's/<p>/\
 /g' | sed 's/<\/p>/\
-/g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed "s/&#039;/\'/g" | sed 's/&quot;/\"/g'| grep "$actualregex" | grep -v '^$' -c)
-      ((sum+=cnt))
+/g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed "s/&#039;/\'/g" | sed 's/&quot;/\"/g'| grep "$actualregex" -o -c )
+((sum+=cnt))
       echo "There are ${cnt} instances of the regex \"$i\" in your Facebook exchanges."
     done
     echo "I found ${sum} instances total."
