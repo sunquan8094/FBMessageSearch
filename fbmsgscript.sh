4413a69df3e499699097f079f774db63e1f3e40d
sunquan8094 <sunquan8094@gmail.com>
2015-06-19 16:59:59 -1000

    Code cleanup and additional support for additional operators

--- fbmsgscript.sh
+++ fbmsgscript.sh
@@ -2,22 +2,28 @@
 # fbmsgscript.sh
 
 filename="messages.htm"
-filefound=$(ls $1 | grep $filename | wc -l)
 
-if [[ -n $1 ]] && [[ -n $2 ]] && [[ $filefound -eq 1 ]]; then
-  sum=0
-  for i in ${*:2:(($#-1))}; do 
-    actualregex=$(echo $i | sed -e 's/\|/\\\|/g')
-    cnt=$(cat $1$filename | sed 's/<p>/\
+if [[ $# -ge 2 ]]; then
+  filefound=$(ls "$1" | grep "$filename" -c)
+  if [[ filedfound > 0 ]]; then
+    sum=0
+    for i in ${*:2:(($#-1))}; do
+      actualregex=$(echo $i | sed -e 's/\|/\\\|/g' | sed -e 's/\?/\\\?/g' | sed -e 's/</\\\</g' | sed -e 's/>/\\\>/g')
+
+  # Code below is used for debugging.
+  #   cat $1$filename | sed 's/<p>/\
+#/g' | sed 's/<\/p>/\
+#/g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed "s/&#039;/\'/g" | sed 's/&quot;/\"/g'| grep "$actualregex" | grep -v '^$'
+
+      cnt=$(cat $1$filename | sed 's/<p>/\
 /g' | sed 's/<\/p>/\
-/g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed 's/ /\
-/g' | grep -e "$actualregex" | wc -l)
+/g' | sed s/\<div.*\>//g | sed 's/<\/div>//g' | sed "s/&#039;/\'/g" | sed 's/&quot;/\"/g'| grep "$actualregex" | grep -v '^$' -c)
       ((sum+=cnt))
-    echo "There are ${cnt} instances of the regex $actualregex in your Facebook exchanges."
-  done
-  echo "I found ${sum} instances total." 
-elif [[ -z $2 ]] || [[ -z $1 ]]; then
+      echo "There are ${cnt} instances of the regex \"$i\" in your Facebook exchanges."
+    done
+    echo "I found ${sum} instances total."
+  else echo "I could not find the file in the specified directory."
+  fi
+else
   echo "You're missing parameters, yo."
-elif [[ $filefound -le 0 ]]; then
-  echo "I could not find the file in the specified directory." 
 fi
